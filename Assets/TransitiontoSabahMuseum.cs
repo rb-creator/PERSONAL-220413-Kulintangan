f6f1cc6087e40f3e328e8d27f11c309d4eeef90e
Rene Mark Barrow <72480653+rb-creator@users.noreply.github.com>
2022-04-21 15:19:46 +0800

    Imported Museum from Sketchup

---
+++ Assets/TransitiontoSabahMuseum.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitiontoSabahMuseum : MonoBehaviour
+{
+    public void LoadSceneUsingName(string KulintanganVR)
+    {
+        SceneManager.LoadScene(KulintanganVR);
+    }
+}
+
+    
